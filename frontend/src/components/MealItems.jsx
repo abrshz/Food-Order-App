cfdc7adffd7bb4da3b2a5c2cc64b11e2e0e6aa8b
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-09-20 20:51:56 +0300

    Formatting currency

--- frontend/src/components/MealItems.jsx
+++ frontend/src/components/MealItems.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { currencyFormatter } from '../utility/formatting'
 
 function MealItems({meal}) {
   return (
@@ -7,8 +8,8 @@ function MealItems({meal}) {
             <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
             <div>
                 <h3>{meal.name}</h3>
-                <p className="meal-item-price">{meal.price}</p>
-                <p className="mela-item-description">{meal.description}</p>
+                <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
+                <p className="meal-item-description">{meal.description}</p>
             </div>
             <p className="meal-item-actions">
                 <button>Add to Cart</button>
