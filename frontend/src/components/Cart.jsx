f9a26fff5a61f6f0edce1860c8eaee90177d4a20
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-10-06 23:13:41 +0300

    Use currency formatter and created UserProgressContext component

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,15 +1,22 @@
 import React, { useContext } from 'react'
 import Modal from './UI/Modal'
 import CartContext from '../store/CartContext'
+import { useCurrencyFormatter } from '../hooks/useCurrencyFormatter'
 
 function Cart() {
-    useContext(CartContext)
+    const cartCtx = useContext(CartContext)
+    const cartTotal = cartCtx.items.reduce((totalPrice , item)=> totalPrice + item.quantity * item.price, 0)
   return (
-    <Modal className='cart'>
+    <Modal className='cart' open={true}>
         <h2>Your Cart</h2>
         <ul>
-
+          {cartCtx.items.map(item => <li key={item.id}>{item.name} - {item.quantity}</li>)}
         </ul>
+        <p className='cart-total'>{useCurrencyFormatter.format(cartTotal)}</p>
+        <p className="modal-actions">
+          <Button textOnly>Close</Button>
+          <Button>Go to Checkout</Button>
+        </p>
     </Modal>
   )
 }
